b53fee1e02895e3911531a0c352fe74de3dfc7f9
rana <jawalerana123@gmail.com>
2022-06-30 07:53:13 +0000

    updated id.sh for password confirm feature

--- id.sh
+++ id.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 read -p "Enter the username" username
 read -sp "Enter the password" passwd
+echo "Please re-enter password"
+read -sp "Enter password again: " passwd1
 echo
 echo "Username is : $username"
+if [ $passwd = $passwd1 ]
+then
 echo "Password is : $passwd"
+else
+echo "password does not match"
+fi

126c011525fbd5568f03a1a74abb3efa19de9419
ram <22091996ram@gmail.com>
2022-06-30 15:42:24 +0530

    update passwd variables

--- id.sh
+++ id.sh
@@ -2,15 +2,15 @@
 #This is get input from user sectertely
 #read -sp to read input secretely
 read -p "Enter the username" username
-read -sp "Enter the password" passwd
+read -sp "Enter the password" passwd1
 echo #hello
 echo "Please re-enter password"
-read -sp "Enter password again: " passwd1
+read -sp "Enter password again: " passwd2
 echo #this echo is move cursor to new line
 echo "Username is : $username"
-if [ $passwd = $passwd1 ]
+if [ $passwd2 = $passwd1 ]
 then
-echo "Password is : $passwd"
+echo "Password is : $passwd2"
 else
 echo "password does not match"
 fi
